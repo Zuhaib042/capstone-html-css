003fc93be51b338e177a6d94c3cfce658e6fe271
Zuhaib Amjad <umar0423@gmail.com>
2022-09-15 09:00:43 +0500

    Add and implemented mobile-menu functionality succesfully

--- js/script.js
+++ js/script.js
@@ -1,10 +1,22 @@
 const hamburger = document.getElementById('menu-btn');
 const menu = document.getElementById('menu');
+const body = document.querySelector('body');
+const links = document.querySelectorAll('.mobile-links');
 
 hamburger.addEventListener('click', () => {
   hamburger.classList.toggle('ham-open');
-  menu.classList.toggle('flex');
   menu.classList.toggle('hidden');
+  menu.classList.toggle('flex');
+  body.classList.toggle('active');
 });
 
+links.forEach((link) => {
+  link.addEventListener('click', () => {
+    hamburger.classList.remove('ham-open');
+    menu.classList.toggle('hidden');
+    menu.classList.toggle('flex');
+    body.classList.toggle('active');
+    console.log('body active');
+  });
+});
 AOS.init();
