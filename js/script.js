a149acb2193e5a9f79fc87d54fe534f800b4c5f2
Zuhaib Amjad <umar0423@gmail.com>
2022-09-16 20:53:56 +0500

    Fix remaining issues mentioned by code reviewer

--- js/script.js
+++ js/script.js
@@ -27,7 +27,7 @@ const speakers = [
     name: 'Osman Khalid Butt',
     profession: 'Drama and Film Actor',
     description:
-      'He is best known for his role as Wali Sohaib Khan in Diyar-E-Dil , Faaz Ibrahim in Chupke Chupke and Malik Shahzain in Ehd-e-Wafa . He is the recipient of several accolades, including two Hum Awards and an ARY Film Award.',
+      'He is best known for his role as Wali Sohaib Khan in Diyar-E-Dil , Faaz Ibrahim in Chupke Chupke and Malik Shahzain in Ehd-e-Wafa .',
     src: './images/osman-khalid2.jpeg',
     alt: 'Actor Osman Khalid Butt',
   },
@@ -55,18 +55,36 @@ const speakers = [
     src: './images/ahmad-ali.jpeg',
     alt: 'Actor Ahmad Ali Akbar',
   },
+  {
+    name: 'Bilal Ashraf',
+    profession: 'Actor , Director and Producer',
+    description:
+      'He is a Pakistani film actor, producer and visual effects director. After studying visual effects from the Franklin Marshall College, he started his career as a visual effects director and then pursued his career in acting after making a brief appearance in the 2014 thriller O21.',
+    src: './images/bilali.jpeg',
+    alt: 'Actor Bilal Ashraf',
+  },
+  {
+    name: 'Shehryar Munawwar',
+    profession: 'Model, Actor and Producer',
+    description:
+      'He is a Pakistani actor, film producer and director. He made his film debut as a lead actor and producer in 2016 with Ho Mann Jahaan. He is notable for his role in Asim Raza"s Parey Hut Love, which became Pakistan"s highest grossing movie in the year 2019',
+    src: './images/shehryar.jpeg',
+    alt: 'Actor Shehryar Munawar',
+  },
 ];
 
 window.onload = () => {
   speakers.forEach((speaker) => {
-    speakersContainer.innerHTML += `<article class="flex items-start ">
-          <div class="card-img">
-              <img src=${speaker.src} alt=${speaker.alt}>
+    speakersContainer.innerHTML += `<article class="flex justify-start">
+          <div class="min-w-[140px] md:flex flex-col">
+          <img class="trans-img-back" src="images/transparent.jpg" alt="transparent image background">
+              <img class="speaker-img" src=${speaker.src} alt=${speaker.alt}>
           </div>
-          <div class="card-content flex flex-col pl-3">
+          <div class="card-content md:ml-4 ml-8">
               <h2 class="card-title text-xl font-bold">${speaker.name}</h2>
-              <h3 class="card-pro pt-1">${speaker.profession}</h3>
-              <p class="w-max-64 md:w-48 text-sm font-light">${speaker.description}</p>
+              <h3 class="card-pro font-medium text-sm pt-1">${speaker.profession}</h3>
+              <div class="line-small my-1"></div>
+              <p class="w-max-64 md:w-48 text-sm card-desc">${speaker.description}</p>
           </div>
       </article>`;
   });
